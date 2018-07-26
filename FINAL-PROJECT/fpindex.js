418a32c837ad00971086df6e3eef4d28280c6d03
Jasper Richards <jasper.richards@sascholar.org>
2018-07-26 17:18:53 +0000

    second commit

--- FINAL-PROJECT/fpindex.js
+++ FINAL-PROJECT/fpindex.js
@@ -2,11 +2,15 @@ console.log("Operating!");
 const thegoal = document.querySelector('#greybox');
 const theplayer = document.querySelector('#player');
 const debody  = document.querySelector('#body');
+const obsbox = document.querySelector('#obstacles');
 
 theplayer.addEventListener('collide', function(e) {
     console.log('Player has collided with body #' + e.detail.body.id);
 });
 thegoal.addEventListener('collide', function(e) {
     console.log("Player has collided with goal");
+});
 
+obsbox.addEventListener('collide', function(e) {
+    console.log("Player has collided obstacle");
 });
